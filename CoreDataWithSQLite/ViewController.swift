31512e8a7f31ab020c2e438a204e935f1ba2cf4f
plak160 <plak160@wp.pl>
2024-05-28 10:05:27 +0200

    working project up to 4.6 commit

--- CoreDataWithSQLite/ViewController.swift
+++ CoreDataWithSQLite/ViewController.swift
@@ -20,10 +20,35 @@ class ViewController: UIViewController {
     override func viewDidLoad(){
         super.viewDidLoad()
         
+        addGroup(name: "Grupa 12K1", code: "12K1")
+        
         labelResult.numberOfLines = 0
         labelResult.text = allResultsString
     }
-
-
+    
+    func updateMessageString(newMessage: String) {
+        allResultsString.append("\n" + newMessage)
+    }
+    
+    func addGroup(name: String, code: String){
+        // 1. Create an Group object
+        let newGroup = Groups(context: context)
+        
+        // 2. Set the properties of that object
+        newGroup.name = name
+        newGroup.code = code
+        
+        // 3. Use the context variable to save the Group to the database table
+        do {
+            try context.save()
+            print("Group saved!")
+            updateMessageString(newMessage: "\n Group \(name) with code \(code) saved!")
+        } catch {
+            print("Saved failed.")
+            updateMessageString(newMessage: "Saved failed.")
+        }
+    }
+    
+   // /Users/plak/Library/Developer/CoreSimulator/Devices/EDDE665B-77D0-4C6E-853C-A64EA957AB72/data/Containers/Data/Application/D3BEE5CE-A251-44A8-9666-DCD7170ACA9B/Library/Application Support/
 }
 
